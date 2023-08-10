6ee85a3589d17f6afe6cb436ffbf050ab9ff9031
DevOpF3loH <devopnthw@gmail.com>
2023-08-10 17:44:17 +0300

    final commit

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+  return ( 
+    <>
+    <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur inventore facere voluptatem modi, quas cum totam dolorem iure accusantium sequi, quia delectus quae soluta obcaecati cupiditate a repellat, consequatur assumenda?</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat rem, reiciendis expedita optio repellendus consequuntur? Accusantium quam reiciendis sit temporibus, saepe natus vitae recusandae rem suscipit, culpa neque qui commodi!</p>
+    </div>
+    </>
+   );
+}
+ 
+export default About;
